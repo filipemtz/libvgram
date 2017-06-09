fc383b6d1068ef70118504c182f72b14f2ac78e5
scart97 <scart.lucas@gmail.com>
2017-06-09 16:09:08 -0300

    Corrected include in mnist example for OpenCV 3

--- example/mnist/read_mnist.h
+++ example/mnist/read_mnist.h
@@ -5,7 +5,8 @@
 #include <string>
 #include <vector>
 #include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
